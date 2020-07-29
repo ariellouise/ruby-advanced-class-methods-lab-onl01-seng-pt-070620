77c0be3ad37e62bb90bfe6ec4059931120e90ff7
ariel murphy <arielladders@gmail.com>
2020-07-29 13:00:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   
   def self.new_from_filename(filename) 
     song = self.new
-    split_filename = filename.chomp
+    split_filename = filename.chomp("mp3")
   end 
   
   def self.create_from_filename
