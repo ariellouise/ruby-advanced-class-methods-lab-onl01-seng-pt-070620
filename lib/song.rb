7a0e6cbd45343c576b58c0ff835b71d028289c17
ariel murphy <arielladders@gmail.com>
2020-07-29 13:03:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
     song
   end 
   
-  def self.create_from_filename
+  def self.create_from_filename(filename)
     song = self.create
     split_filename = filename.chomp("mp3").split(" -")
     song.name = split_filename[1]
